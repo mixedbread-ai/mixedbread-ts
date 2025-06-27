6612b0441afd33d20dadd8bfe783859312505fce
Naing Linn Khant <nainglinnkhnt@gmail.com>
2025-06-27 21:49:12 +0700

    Remove cli and mcp server (#198)
    * Reapply "chore: remove cli and mcp-server from release flow"
    
    This reverts commit b1fbdbe3a6d0436886a9568054ff1362d98334c4.
    
    * chore: remove cli and mcp-server packages

--- packages/cli/src/index.ts
+++
@@ -1,5 +0,0 @@
-// Main CLI exports
-export { createVectorStoreCommand } from './commands/vector-store';
-export { createClient } from './utils/client';
-export { loadConfig, saveConfig, getApiKey } from './utils/config';
-export { formatOutput } from './utils/output';
